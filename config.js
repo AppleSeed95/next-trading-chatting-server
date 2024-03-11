aaacb0c5c50017b3905473ac2941470eac9c8f02
Kyosuke <everlastingfriendship194@gmail.com>
2024-03-12 00:29:40 +0900

    first

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    main_url:'http://localhost:3000/api/chatting'
+}
---
+++ controller.js
@@ -0,0 +1,25 @@
+// const rp = require("request-promise")
+const request = require("request-promise");
+const config = require('./config')
+module.exports = (io, data) => {
+    const { roomId, userId, msg } = data;
+    const today = new Date();
+    const day = `${today.getFullYear()}/${today.getMonth() + 1
+        }/${today.getDate()}`;
+    const time = `${today.getHours()}:${today.getMinutes()}`
+    const options = {
+        method: 'POST',
+        uri: config.main_url, // Replace with the target server's URL
+        body: {
+            roomId, userId, msg, day, time
+        },
+        json: true, // Set to true if you want to send JSON data in the request body
+    };
+    request(options)
+        .then((response) => {
+            io.to(data.roomId).emit('message'); // Broadcast the message to all connected clients
+        })
+        .catch((error) => {
+            io.to(data.roomId).emit('message'); // Broadcast the message to all connected clients
+        });
+}

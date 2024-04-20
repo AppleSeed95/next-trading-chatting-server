dfb5be2f80e0f70df7e726aee2ea77af9129d248
AppleSeed <everlastingfriendship194@gmail.com>
2024-04-21 01:39:08 +0900

    fix:message view

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = require('socket.io').listen(server)
 
 io.on('connection', (socket) => {
     socket.on('message', (data) => {
-        console.log('message');
+        console.log('message', data);
         controller(io, data);
     });
     socket.on('info', (data) => {
